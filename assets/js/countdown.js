c2990386cc2635dc3967de93d0544c2e48069270
Anass Bakkour <anass.bakkour@uit.ac.ma>
2024-11-10 12:55:13 +0100

    Delete assets/js/countdown.js

--- assets/js/countdown.js
+++
@@ -1,2 +0,0 @@
-<script src="https://cdn.logwork.com/widget/countdown.js"></script>
-<a href="https://logwork.com/countdown-timer" class="countdown-timer" data-style="flip3" data-timezone="Africa/Casablanca" data-date="2025-01-01 12:39">Countdown Timer</a>
